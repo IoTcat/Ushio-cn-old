b0054a5b5390d0c9fdc1f216f532fa16d8f38f74
iotcat <i@iotcat.me>
2019-06-29 18:01:10 +0800

    auto update

--- yimian/iot/mqtt/smartfarm/mqtt-server.js
+++ yimian/iot/mqtt/smartfarm/mqtt-server.js
@@ -2,6 +2,9 @@ const mosca = require('mosca');
 const mysql = require('mysql');
 const fs = require('fs');
 
+
+var g_MaxWaitTime = 10000
+
 var settings = {
     port: 30032
 };
@@ -23,13 +26,63 @@ connection.connect();
 server.on('published', function (packet, client) {
     switch (packet.topic) {
         case 'res/node0/status':
-            console.log(packet.payload.toString());
+            //console.log(packet.payload.toString());
+            pushElement('status', packet.payload.toString(), node0Cache, storeNode0Data);
+            break;
+        case 'res/node0/batteryLevel':
+            //console.log(packet.payload.toString());
+            pushElement('batteryLevel', packet.payload.toString(), node0Cache, storeNode0Data);
+            break;
+        case 'res/node0/waterSwitch':
+            //console.log(packet.payload.toString());
+            pushElement('waterSwitch', packet.payload.toString(), node0Cache, storeNode0Data);
             break;
 
+        case 'res/node0/temperature':
+            //console.log(packet.payload.toString());
+            pushElement('temperature', packet.payload.toString(), node0Cache, storeNode0Data);
+            break;
+        case 'res/node0/humidity':
+            //console.log(packet.payload.toString());
+            pushElement('humidity', packet.payload.toString(), node0Cache, storeNode0Data);
+            break;
+
+        case 'res/node1/status':
+            //console.log(packet.payload.toString());
+            pushElement('status', packet.payload.toString(), node1Cache, storeNode1Data);
+            break;
+        case 'res/node1/batteryLevel':
+            //console.log(packet.payload.toString());
+            pushElement('batteryLevel', packet.payload.toString(), node1Cache, storeNode1Data);
+            break;
+        case 'res/node1/waterSwitch':
+            //console.log(packet.payload.toString());
+            pushElement('waterSwitch', packet.payload.toString(), node1Cache, storeNode1Data);
+            break;
+
+        case 'res/node1/temperature':
+            //console.log(packet.payload.toString());
+            pushElement('temperature', packet.payload.toString(), node1Cache, storeNode1Data);
+            break;
+        case 'res/node1/humidity':
+            //console.log(packet.payload.toString());
+            pushElement('humidity', packet.payload.toString(), node1Cache, storeNode1Data);
+            break;
+ 
     }
 });
 
-var node0PackageCache = {
+var node0Cache = {
+    status: null,
+    batteryLevel: null,
+    waterSwitch: null,
+    temperature: null,
+    humidity: null,
+    BeginTime: null,
+    EndTime: null,
+}
+
+var node1Cache = {
     status: null,
     batteryLevel: null,
     waterSwitch: null,
@@ -41,13 +94,41 @@ var node0PackageCache = {
 
 
 
-var node0Package = function(t, s){
 
-    if(node0PackageCache.every(function(r){
-	
-	return !r;
-    })) console.log('sss');
-}();
+var pushElement = function(t, s, o, f){
+    if(o.BeginTime && Date.parse(new Date()) - o.BeginTime > g_MaxWaitTime) objReset(o);
+
+    if(getNumInObj(o, null) == Object.keys(o).length){
+        o.BeginTime = Date.parse(new Date());
+    }
+
+    if(o.hasOwnProperty(t))  o[t] = s;
+
+    if(getNumInObj(o, null) == 1){
+        o.EndTime = Date.parse(new Date());
+        console.log(o);
+	f(o);
+    }
+
+};
+
+var objReset = function(o){
+
+    for(i in o){
+
+        o[i] = null;
+    }
+}
+
+
+var getNumInObj = function(o, t){
+    var c = 0;
+    for(i in o){
+
+        if(o[i] == t) c ++;
+    }
+    return c;
+}
 
 var sql = 'SELECT * FROM api';
 
@@ -59,4 +140,19 @@ connection.query(sql,function (err, result) {
     console.log(result);
 });
 
+var storeNode0Data = (o)=>{storeNodeData(0, o)};
+var storeNode1Data = (o)=>{storeNodeData(1, o)};
 
+var storeNodeData = function(id, o){
+	
+var  addSql = 'INSERT INTO node(timestamp,id,status,qos,batterylevel,waterswitch,temperature,humidity) VALUES(?,?,?,?,?,?,?,?)';
+	var  addSqlParams = [new Date(o.BeginTime).toISOString().slice(0, 19).replace('T', ' '), id, o.status, 1, o.batteryLevel, o.waterSwitch, o.temperature, o.humidity];
+
+	connection.query(addSql,addSqlParams,function (err, result) {
+	        objReset(o);
+		if(err){
+	         console.log('[INSERT ERROR] - ',err.message);
+	         return;
+	        }
+	});
+}
