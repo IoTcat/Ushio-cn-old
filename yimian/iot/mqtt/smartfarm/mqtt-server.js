091902ddf9f977c3108986a680c6d46032fa9f8a
iotcat <i@iotcat.me>
2019-06-29 15:01:07 +0800

    auto update

--- yimian/iot/mqtt/smartfarm/mqtt-server.js
+++ yimian/iot/mqtt/smartfarm/mqtt-server.js
@@ -1,4 +1,6 @@
 const mosca = require('mosca');
+const mysql = require('mysql');
+const fs = require('fs');
 
 var settings = {
     port: 30032
@@ -6,4 +8,55 @@ var settings = {
 
 var server = new mosca.Server(settings);
 
-//server.on();
+var connection = mysql.createConnection({     
+  host     : 'cn.db.yimian.xyz',       
+  user     : 'smartfarm',              
+  password : fs.readFileSync("smartfarm.db.key").toString().replace(/\s+/g,""),
+  port: '3306',                   
+  database: 'smartfarm' 
+}); 
+
+connection.connect();
+ 
+
+
+server.on('published', function (packet, client) {
+    switch (packet.topic) {
+        case 'res/node0/status':
+            console.log(packet.payload.toString());
+            break;
+
+    }
+});
+
+var node0PackageCache = {
+    status: null,
+    batteryLevel: null,
+    waterSwitch: null,
+    temperature: null,
+    humidity: null,
+    BeginTime: null,
+    EndTime: null,
+}
+
+
+
+var node0Package = function(t, s){
+
+    if(node0PackageCache.every(function(r){
+	
+	return !r;
+    })) console.log('sss');
+}();
+
+var sql = 'SELECT * FROM api';
+
+connection.query(sql,function (err, result) {
+        if(err){
+          console.log('[SELECT ERROR] - ',err.message);
+          return;
+        }
+    console.log(result);
+});
+
+
