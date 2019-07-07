b926ae0f2c65a684d9120617c300d6bb97c019a2
iotcat <i@iotcat.me>
2019-07-08 00:01:08 +0800

    auto update

--- ushio/www/api/getVisitNum.php
+++ ushio/www/api/getVisitNum.php
@@ -8,6 +8,8 @@ $conn = db__connect("log");
 $img = db__rowNum($conn, "log_api", "api", "img");
 $msc = db__rowNum($conn, "log_api", "api", "msc");
 $words = db__rowNum($conn, "log_api", "api", "words");
+$mail = db__rowNum($conn, "log_api", "api", "mail");
+$gugu = db__rowNum($conn, "log_api", "api", "gugu");;
 
-echo json_encode(array("img"=>$img, "msc"=>$msc, "words"=>$words));
+echo json_encode(array("img"=>$img, "msc"=>$msc, "words"=>$words, "mail"=>$mail, "gugu"=>$gugu));
 
