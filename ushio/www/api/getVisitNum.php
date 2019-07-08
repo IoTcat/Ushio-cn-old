60e05cc22dfcd810ab1bfd32a31746884297dc9b
iotcat <i@iotcat.me>
2019-07-08 15:03:13 +0800

    auto update

--- ushio/www/api/getVisitNum.php
+++ ushio/www/api/getVisitNum.php
@@ -2,10 +2,10 @@
 include 'functions.php';
 
 /* anti ddos */
-if(!isset($_COOKIE['_token__']) || $_COOKIE['_token__'] != md5(date('Y-m-d-H').$_SERVER['REMOTE_ADDR'].$_SERVER['HTTP_USER_AGENT'])) {
-    setcookie("_token__",md5(date('Y-m-d-H').$_SERVER['REMOTE_ADDR'].$_SERVER['HTTP_USER_AGENT']),time()+1*3600);
+/*if(!isset($_COOKIE['_token__']) || $_COOKIE['_token__'] != md5(date('Y-m-d-H'))) {
+    setcookie("_token__",md5(date('Y-m-d-H')),time()+1*3600);
     header("Location: https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'], true, 301);
-}
+}*/
 
 
 header("Access-Control-Allow-Origin: *");
