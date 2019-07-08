057aadc151c1a81d39d72004cced4fa9a2cdb2f4
iotcat <i@iotcat.me>
2019-07-09 00:01:08 +0800

    auto update

--- ushio/www/api/img/getInfo.php
+++ ushio/www/api/img/getInfo.php
@@ -1,16 +1,8 @@
 <?php
-
 include '../functions.php';
 
+$type = $_REQUEST['type'];
 
-function getImgsInfo($type){
-    $arr_os = array();
-    $arr = array();
+$arr = getImgsInfo($type);
 
-    exec('obs ls obs://yimian-image/koino', $arr_os);
-    $str = implode ($arr_os);
-    preg_match_all('/img_(\S*?)_(\d{2,4})x(\d{2,4})_(\S*?).(jpe?g|png|gif|svg)\b/', $str, $arr);
-
-    return $arr;
-}
-
+echo json_encode($arr);
