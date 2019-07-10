6ed41cf40a28513e688414bbd92666d23332b557
iotcat <i@iotcat.me>
2019-07-10 21:01:07 +0800

    auto update

--- ushio/www/imgbed/upload.php
+++ ushio/www/imgbed/upload.php
@@ -15,20 +15,24 @@ foreach($file['name'] as $key=>$val){
         $o['code'] = "500";
 	break;
     }
-    $typeArr = explode("/", $file["type"][$key]);
+    $imginfo = getimagesize($file["tmp_name"][$key]); 
+    $typeArr = explode("/", $imginfo["mime"]);
     if($typeArr[0]== "image"){
         $imgType = array("png","jpg","jpeg","gif","svg");
         if(in_array($typeArr[1], $imgType)){
-           $imginfo = getimagesize($file["tmp_name"][$key]); 
            $imgpath = "/home/ushio/www/imgbed/upload/imgbed/";
            $imgname = "img_".substr(md5(time().rand()),0,8)."_".$imginfo[0]."x".$imginfo[1]."_".$imginfo['bits']."_null_normal.".$typeArr[1];
           if(!move_uploaded_file($file["tmp_name"][$key], $imgpath.$imgname)){
 		$o["code"] = "550";
+                break;
 	  }
            
 	  $o["url"].="https://api.yimian.xyz/img/?path=imgbed/".$imgname."\n\n";
         }
 
+   }else{
+       $o["code"] = "560";
+       break;
    }
 }
 
