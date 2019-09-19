414215221a86a0f1feaf0b755ce5f1b021a127f4
iotcat <i@iotcat.me>
2019-09-19 18:01:05 +0800

    auto update

--- yimian/www/settlement/imgbed/js/main.js
+++ yimian/www/settlement/imgbed/js/main.js
@@ -45,10 +45,10 @@ function select(){
 (function(){
     var options = {};
     $('.js-uploader__box').uploader({
-    	'selectButtonCopy':'请选择或拖拽文件',
-    	'instructionsCopy':'你可以选择或拖拽多个文件',
-    	'submitButtonCopy':'上传选择的文件',
-    	'furtherInstructionsCopy':'你可以选择或拖拽更多的文件',
-    	'secondarySelectButtonCopy':'选择更多的文件'
+    	'selectButtonCopy':'press to select',
+    	'instructionsCopy':'select or drag one pic here',
+    	'submitButtonCopy':'upload',
+    	'furtherInstructionsCopy':'',
+    	'secondarySelectButtonCopy':'select more'
     });
 }());
--- yimian/www/settlement/imgbed/upload.php
+++ yimian/www/settlement/imgbed/upload.php
@@ -20,14 +20,14 @@ foreach($file['name'] as $key=>$val){
     if($typeArr[0]== "image"){
         $imgType = array("png","jpg","jpeg","gif","svg");
         if(in_array($typeArr[1], $imgType)){
-           $imgpath = "/home/ushio/www/imgbed/upload/imgbed/";
+           $imgpath = "/home/yimian/www/settlement/imgbed/upload/invoice/";
            $imgname = "img_".substr(md5(time().rand()),0,8)."_".$imginfo[0]."x".$imginfo[1]."_".$imginfo['bits']."_null_normal.".$typeArr[1];
           if(!move_uploaded_file($file["tmp_name"][$key], $imgpath.$imgname)){
 		$o["code"] = "550";
                 break;
 	  }
            
-	  $o["url"].="https://api.yimian.xyz/img/?path=imgbed/".$imgname."\n\n";
+	  $o["url"].="https://api.yimian.xyz/img/?path=invoice/".$imgname."\n\n";
         }
 
    }else{
@@ -39,5 +39,5 @@ foreach($file['name'] as $key=>$val){
 
 echo json_encode($o);
 
-exec('obs cp /home/ushio/www/imgbed/upload/imgbed/ obs://yimian-image/ -r -f');
-exec('rm -rf /home/ushio/www/imgbed/upload/imgbed/*');
+exec('obs cp /home/yimian/www/settlement/imgbed/upload/invoice/ obs://yimian-image/ -r -f');
+exec('rm -rf /home/yimian/www/settlement/imgbed/upload/invoice/*');
