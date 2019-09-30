ee664bb5754b63c16dfc91733f41b119ec98a89e
iotcat <i@iotcat.me>
2019-10-01 06:01:05 +0800

    auto update

--- yimian/www/shorturl/index.php
+++ yimian/www/shorturl/index.php
@@ -4,11 +4,25 @@ include '/home/yimian/www/shorturl/functions.php';
 
 
 Header("HTTP/1.1 301 Moved Permanently"); 
-
+echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING'];
 
 if(!isset($_REQUEST['s'])) {
     Header("Location: https://www.eee.dog/");
     die();
 }
 
-$keyword = $_REQUEST['keyword'];
+$keyword = substr($_REQUEST['s'], 1);
+
+$conn = db__connect();
+
+$res = db__getData($conn, "reflect", "keyword", $keyword);
+
+
+if($res === 404){
+    Header("Location: https://www.eee.dog/");
+    die();
+}
+
+$cmd = "Location: ".$res[0]['url'];
+Header($cmd);
+die();
