e27f615dccd50b2120d34c914db1e738a3861c2c
iotcat <i@iotcat.me>
2019-11-24 00:01:08 +0800

    auto update

--- ushio/www/api/msc/index.php
+++ ushio/www/api/msc/index.php
@@ -98,7 +98,7 @@ if($type == "single"){
 
 if($type == "playlist"){
 	/* 7.18 events */
-	$id="2889727316";
+	//$id="2889727316";
 	if(!isset($id)){
 		echo json_encode(array("code"=>500, "err"=>"You need to provide an id!!"));
 		die();
