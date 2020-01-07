2f0b91c2df72f6e41304bbefd7e8b264b1af790c
iotcat <i@iotcat.me>
2020-01-08 00:01:06 +0800

    auto update

--- ushio/www/api/sms/index.php
+++ ushio/www/api/sms/index.php
@@ -2,4 +2,21 @@
 
 include '../functions.php';
 
-//yimian__sms("18118155257", 1, "原子弹", "阵亡","");
+header('Access-Control-Allow-Origin:*');
+
+$to = $_REQUEST['to'];
+$template = $_REQUEST['template'];
+$s0 = $_REQUEST['s0'];
+$s1 = $_REQUEST['s1'];
+$s2 = $_REQUEST['s2'];
+$t = $_REQUEST['t'];
+
+if(!isset($to)) die('Target Tel not Specified!!');
+if(!isset($t) || $t < time() - 10) die('Illegal Visit!!!');
+if(!isset($template)) $template = 1;
+if(!isset($s0)) $s0 = "";
+if(!isset($s1)) $s1 = "";
+if(!isset($s2)) $s2 = "";
+
+
+yimian__sms($to, $template, $s0, $s1, $s2);
