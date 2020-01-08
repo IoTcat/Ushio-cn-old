5189de4317585f65daffa32e31d9d25653efe650
iotcat <i@iotcat.me>
2020-01-08 09:01:53 +0800

    auto update

---
+++ yimian/www/nextcloud/config/config.php
@@ -0,0 +1,23 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocgjwkzvdhia',
+  'passwordsalt' => 'oI13FMa4JRQDWpAfcdEgdfS0WGKYV2',
+  'secret' => 'NofvPgEhyWYEeJWTfNVCdiG9ExXGbOFvqgZJHeTiuJ+eUiTw',
+  'trusted_domains' => 
+  array (
+    0 => 'cloud.yimian.xyz',
+  ),
+  'datadirectory' => '/home/yimian/www/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '17.0.2.1',
+  'overwrite.cli.url' => 'https://cloud.yimian.xyz',
+  'dbname' => 'cloud',
+  'dbhost' => '192.168.0.90',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'cloud',
+  'dbpassword' => 'enedk^d%edl9f3eFen',
+  'installed' => true,
+  'maintenance' => false,
+);
