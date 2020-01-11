68b4db9ce3cd6e3507db25adf5b1ce517de9ceff
iotcat <i@iotcat.me>
2020-01-11 09:01:17 +0800

    auto update

--- yimian/www/acg.watch/index.php
+++ yimian/www/acg.watch/index.php
@@ -11,6 +11,7 @@ $res = db__getData(db__connect(), 'video');
 <script src="https://cdn.yimian.xyz/ushio-js/ushio-head.min.js"></script>
 </head>
 <body>
+<h2><script>document.write('<a href="https://acg.watch/player/?url='+cookie.get('last_watch')+'">继续播放上次位置</a>');</script></h2>
 <?php
 foreach($res as $i){
     echo '<a href="https://acg.watch/player/?url='.$i['url'].'" >'.$i['name'].'</a><br/>';
