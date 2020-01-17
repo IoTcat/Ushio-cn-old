1fddca2bca6a3d0ab9571cafef3952e4ca95b04c
iotcat <i@iotcat.me>
2020-01-17 17:49:24 +0000

    add acg.watch index.php

--- yimian/www/acg.watch/index.php
+++ yimian/www/acg.watch/index.php
@@ -3,21 +3,81 @@ include './functions.php';
 
 $res = db__getData(db__connect(), 'video');
 
-?>
+function getKeys($arr, $word){
+	$arr_word = [];
+	foreach($arr as $i){
+		if(!in_array($i[$word], $arr_word)){
+			array_push($arr_word, $i[$word]);
+		}
+	}
+	return $arr_word;
+};
 
+$classes = getKeys($res, "class");
+
+?>
+<!DOCTYPE html>
 <html>
 <head>
-<title>ACG WATCH</title>
-<script src="https://cdn.yimian.xyz/ushio-js/ushio-head.min.js"></script>
+	<meta charset="UTF-8">
+	<title>ACG WATCH</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/awsm.css@3.0.4/dist/awsm_theme_pearl-lusta.min.css">
+	<script type="text/javascript">block_aplayer = true;</script>
+	<script src="https://cdn.yimian.xyz/ushio-js/ushio-head.min.js"></script>
 </head>
 <body>
-<h2><script>document.write('<a href="https://acg.watch/player/?url='+cookie.get('last_watch')+'">继续播放上次位置</a>');</script></h2>
+	<header>
+		<h1>ACG.WATCH</h1>
+		<p>Watch ACG video <abbr title="At ACG.WATCH">ONLINE</abbr></p>
+		<nav>
+			<ul>
+				<li><a href="./" aria-hidden="false">Homepage</a></li>
+				<li><a href="./">Playlist</a></li>
+				<li><a href="https://iotcat.me/">About Me</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<article>
+			<section>
+				<aside>
+					<p>
+						<strong>S.F.</strong>
+						<script>document.write('<a href="https://acg.watch/player/?url='+cookie.get('last_watch')+'">Continue Your Last Watch</a>');</script>
+					</p>
+				</aside>
+			</section>
 <?php
-foreach($res as $i){
-    echo '<a href="https://acg.watch/player/?url='.$i['url'].'" >'.$i['name'].'</a><br/>';
-}
 
+	foreach($classes as $class){
+		echo "
+			<section>
+				<h2>".$class."</h2>";				
+		foreach(getKeys($res, "series") as $i){
+			echo "
+				<details>
+					<summary>".$i."</summary>
+					<ul>";
+			foreach($res as $ii){
+				if($ii["series"] == $i){
+					echo "
+						<li><a href=\"./player/?url=".$ii["url"]."\">".$ii["name"]."</a></li>";
+				}
+			}
+			echo "
+					</ul>
+				</details>";
+		}
+		echo "
+		    </section>";
+	}
 ?>
-<script src="https://cdn.yimian.xyz/ushio-js/ushio-footer.min.js"></script>
+		</article>
+	</main>
+    <footer>
+    	<p style="text-align: center;"> Powered By Ushio | Made with love by <a href="https://iotcat.me">iotcat</a> </p>
+    	<br>
+    </footer>
+	<script src="https://cdn.yimian.xyz/ushio-js/ushio-footer.min.js"></script>
 </body>
 </html>
